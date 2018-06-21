5a3425acbe7b4b029a6fa5ed5dd24586749a8919
glietzen@gmail.com <glietzen@gmail.com>
2018-06-20 20:29:54 -0500

    push

--- bucketlist/server.js
+++ bucketlist/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+// const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 app.use(express.static("client/build"));
 
-app.use(routes);
+// app.use(routes);
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
