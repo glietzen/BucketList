1ede9599367e4b35a0b6c81b07c800693a724bd0
Chris B <beesleychc@gmail.com>
2018-06-23 11:23:51 -0500

    changed files from bucketlist to bl2

--- bucketlist2/client/src/App.js
+++ bucketlist2/client/src/App.js
@@ -1,40 +1,15 @@
 import React, { Component } from 'react';
-
-import logo from './logo.svg';
-
 import './App.css';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Home from "./components/pages/Home";
 
-class App extends Component {
-  state = {
-    response: ''
-  };
 
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.response}</p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home}/>
+    </div>
+  </Router>
+);
 
 export default App;
