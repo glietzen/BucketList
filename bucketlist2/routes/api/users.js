43a51155ec06ce87f473e781fd8bff6d51d2a293
Garrett Lietzen <glietzen@gmail.com>
2018-06-29 17:34:15 -0500

    registration and login validation
    finished registration form and login form validation via api

--- bucketlist2/routes/api/users.js
+++ bucketlist2/routes/api/users.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 
 // LOAD INPUT VALIDATION
 const validateRegisterInput = require('./../../validation/register');
+const validateLoginInput = require('./../../validation/login');
 
 router.get('/test', (req,res) => res.json({msg: 'Users Works'}));
 
@@ -45,15 +46,27 @@ router.post('/register', (req,res) => {
 })
 
 router.post('/login', (req,res) => {
+
+    const {errors, isValid} = validateLoginInput(req.body);
+
+    // CHECK VALIDATION
+    if(!isValid) {
+        return res.status(400).json(errors)
+    }
+
+
     const email = req.body.email;
     const password = req.body.password;
+
+    
 
     // FIND USER BY EMAIL
     User.findOne({email})
         .then(user => {
             // CHECK FOR USER
             if(!user) {
-                return res.status(404).json({email: 'User not found'});
+                errors.email = 'User not found';
+                return res.status(404).json(errors);
             }
 
             // CHECK PASSWORD
@@ -74,7 +87,8 @@ router.post('/login', (req,res) => {
                             })
                         });
                     } else {
-                        return res.status(400).json({password: 'Password incorrect'});
+                        errors.password = 'Password incorrect';
+                        return res.status(400).json(errors);
                     }
                 });
 
