948921947cfaedfc76ce7ccdb29f6d889904071d
Chris B <beesleychc@gmail.com>
2018-07-07 09:52:18 -0500

    connected to Database

--- bucketlist2/client/src/components/pages/Home/Home.js
+++ bucketlist2/client/src/components/pages/Home/Home.js
@@ -4,53 +4,67 @@ import Cardlist from "../../Cardlist/Cardlist";
 import {Component} from 'react';
 import {PropTypes} from 'prop-types';
 import {connect} from 'react-redux';
-import {Row, Col} from 'react-materialize';
+import {Row} from 'react-materialize';
 import './Home.css';
 import Axios from "axios";
 import {logoutUser} from './../../../actions/authActions';
+import API from '../../../utils/API';
 
 class Home extends Component {
+    
+    state = {
+        itemsArray: [],
+        title: '',
+        location: '',
+        description: ''
+    }
+    
 
     componentDidMount() {
         if(this.props.auth.isAuthenticated) {
             this.props.history.push('/home')
+            console.log(this.props.auth.user.list);
+            
+            this.props.auth.user.list ? this.getUserList(this.props.auth.user.list) : null;
+
         }
     }
+    getUserList = (userID) => {
+        Axios.get(`/api/lists/${userID}`)
+            .then((res) => {
+               if(res.data.items.length > 0) {
+                    this.setState({
+                        itemsArray: res.data.items
+                    });
+               };
+            })
+            .catch(err => console.log(err));        
+    };
+
+    handleNewItemSubmit = event => {
+        event.preventDefault();
+        if (!this.state.title || !this.state.location || !this.state.description) {
+            API.saveItem({
+                title: this.state.title,
+                location: this.state.location,
+                description: this.state.description
+            })
+                .then(res => this.getUserList(this.props.auth.user.list))
+                .catch(err => console.log(err));
+        }
+    };
 
 
     render() {
-        
-        let itemsArrary = [];
+    
 
-        let getUserList = (userID) => {
-
-            console.log(userID)
-            Axios.get(`/api/lists/${userID}`)
-                .then((res) => {
-                   if(res.data.items.length > 0) {
-                    console.log(res.data.items.length)
-                    itemsArrary = res.data.items;
-                    console.log(itemsArrary)
-                   } 
-                });        
-        }
-
-        const {isAuthenticated, user} = this.props.auth;
+        const {isAuthenticated} = this.props.auth;
 
         const authContent = (
             <div className="container">
-                <Col s={6} m={6} l={4}><Cardlist /></Col>
-                <Col s={6} m={6} l={4}><Cardlist /></Col>
-                <Col s={6} m={6} l={4}><Cardlist /></Col>
-        
-                <Col s={6} m={6} l={4}><Cardlist /></Col>
-                <Col s={6} m={6} l={4}><Cardlist /></Col>
-                <Col s={6} m={6} l={4}><Cardlist /></Col>
-        
-                <Col s={6} m={6} l={4}><Cardlist /></Col>
-                <Col s={6} m={6} l={4}><Cardlist /></Col>
-                <Col s={6} m={6} l={4}><Cardlist /></Col>
-                {user.list ? getUserList(user.list) : null}
+                <Cardlist 
+                    itemsArray={this.state.itemsArray}
+                /> 
             </div>
         )
 
@@ -62,7 +76,12 @@ class Home extends Component {
 
         return (
             <div className="background-color">
-                <Nav />
+                <Nav 
+                    handleNewItemSubmit={this.handleNewItemSubmit} 
+                    title={this.state.title}
+                    location={this.state.location}
+                    description={this.state.description}   
+                />
                     <Row>
                 {isAuthenticated ? authContent : guestContent}
                     </Row>
@@ -74,6 +93,7 @@ class Home extends Component {
 Home.propTypes = {
     logoutUser: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired
+    
 }
 
 const mapStateToProps = (state) => ({
