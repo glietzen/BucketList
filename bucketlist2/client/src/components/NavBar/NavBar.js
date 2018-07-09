19bb42fdd15d4093b6f1dd1730eaf111957fccad
RalphWiley <nazkeith@gmail.com>
2018-07-09 14:19:51 -0500

    closing branch

--- bucketlist2/client/src/components/NavBar/NavBar.js
+++ bucketlist2/client/src/components/NavBar/NavBar.js
@@ -6,10 +6,13 @@ import img from './bucket.png';
 
 
 const Nav = () => (
-    <Navbar className="nav-bar teal lighten-2" brand='BucketList'>
-        <NavItem><img className="logo" src={img} height="20px" width="20px"/></NavItem>
-        <NavItem className="nav-item" href='get-started.html'><Modalform /></NavItem>
-        <NavItem className="nav-item" href='components.html'>COMPONENTS</NavItem>
+    <Navbar className="nav-bar teal lighten-2">
+        <div>
+            <span className="title">BucketList</span>
+            <img className="logo" src={img} height="25px" width="25px" />
+            <NavItem className="nav-item" href='get-started.html'><Modalform /></NavItem>
+            <NavItem className="nav-item" href='components.html'>COMPONENTS</NavItem>
+        </div>
     </Navbar>
 );
 
